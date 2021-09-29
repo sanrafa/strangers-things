877ce80d29e6009a7f87702ca29c09b13d338870
Rafa Brown <shane.rafael@zohomail.com>
2021-09-28 20:23:51 -0600

    Import packages & add file structure

---
+++ src/components/index.js
@@ -0,0 +1,1 @@
+// to re-export components
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Router, Link } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("app"));
