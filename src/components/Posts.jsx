98c4d6d08446ddca6dc37562d03ee362664e1318
Rafa Brown <shane.rafael@zohomail.com>
2021-10-03 23:18:19 -0600

    Complete search bar on Posts component

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, Fragment } from "react";
+import React, { useContext, Fragment, useState, useEffect } from "react";
 import { Link, Route, useRouteMatch } from "react-router-dom";
 import { FeaturedPost } from ".";
 
@@ -12,13 +12,68 @@ const Posts = (props) => {
 
   const { token, activeUser } = useContext(UserContext);
 
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredPosts, setFilteredPosts] = useState([]);
+
+  const handleSearch = (search, postStr) => {
+    function escRegEx(str) {
+      return str.replace(/[-\\^$*+?.()|[\]{}]/g, "");
+    }
+    const escapedTerm = escRegEx(search);
+    const regex = new RegExp(escapedTerm, "gi");
+    return regex.test(postStr);
+  };
+
+  useEffect(() => {
+    const searchPosts = posts.filter(
+      (post) =>
+        handleSearch(searchTerm, post.title) ||
+        handleSearch(searchTerm, post.description)
+    );
+    setFilteredPosts(searchPosts);
+  }, [searchTerm]);
+
   return (
     <Fragment>
       <section>
         <h1>Posts</h1>
         <Link to="/newpost">Create new post</Link>
-        {posts
+        <form>
+          <input
+            type="text"
+            placeholder="Search"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          ></input>
+        </form>
+        {posts && !(filteredPosts.length > 0)
           ? posts.map((post) => (
+              <div key={post._id}>
+                <h3>{post.title}</h3>
+                <p>{post.description}</p>
+                {post.isAuthor === true ? (
+                  <button
+                    type="button"
+                    onClick={() =>
+                      deletePost(post._id, token).then(() => {
+                        const activePosts = posts.filter(
+                          (post) => post.active === true
+                        );
+                        setPosts(activePosts);
+                      })
+                    }
+                  >
+                    DELETE
+                  </button>
+                ) : null}
+                {activeUser && !post.isAuthor ? (
+                  <Link to={`/posts/${post._id}`}>Send a message</Link>
+                ) : null}
+                <Link to={`/posts/${post._id}/post`}>View post</Link>
+              </div>
+            ))
+          : posts && filteredPosts.length > 0
+          ? filteredPosts.map((post) => (
               <div key={post._id}>
                 <h3>{post.title}</h3>
                 <p>{post.description}</p>
